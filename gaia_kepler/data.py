408c6b529754f4fc136daa721580ad80c9f8db98
Miguel de Val-Borro <miguel@archlinux.net>
2016-10-18 11:09:29 -0400

    Added docstrings to catalog classes
    Replace planets by exoplanets (according to http://exoplanetarchive.ipac.caltech.edu/docs/program_interfaces.html)

--- gaia_kepler/data.py
+++ gaia_kepler/data.py
@@ -130,9 +130,15 @@ class ExoplanetArchiveCatalog(Catalog):
         df.to_hdf(self.filename, self.name, format="t")
 
 class KICatalog(ExoplanetArchiveCatalog):
+    """
+    Kepler Stellar Table (Q1 through Q17)
+    """
     name = "q1_q17_dr24_stellar"
 
 class KOICatalog(ExoplanetArchiveCatalog):
+    """
+    Kepler Kepler Objects of Interest (Q1 through Q17)
+    """
     name = "q1_q17_dr24_koi"
 
     def join_stars(self, df=None):
@@ -142,12 +148,21 @@ class KOICatalog(ExoplanetArchiveCatalog):
         return pd.merge(df, kic.df, on="kepid")
 
 class CumulativeCatalog(ExoplanetArchiveCatalog):
+    """
+    Kepler Objects of Interest (Cumulative)
+    """
     name = "cumulative"
 
 class ExoplanetCatalog(ExoplanetArchiveCatalog):
-    name = "planets"
+    """
+    All confirmed planets
+    """
+    name = "exoplanets"
 
 class EPICatalog(ExoplanetArchiveCatalog):
+    """
+    K2 Targets
+    """
     ext = ".csv"
     name = "k2targets"
 
@@ -156,6 +171,9 @@ class EPICatalog(ExoplanetArchiveCatalog):
             f.write(file_handle.read())
 
 class K2CandidatesCatalog(ExoplanetArchiveCatalog):
+    """
+    K2 Candidates
+    """
     name = "k2candidates"
 
 class CatalogDownloadError(Exception):
